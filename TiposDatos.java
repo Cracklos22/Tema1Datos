35a7934d8ac8c47e2fa77b6e54b716bece265203
Cracklos22 <132421709+Cracklos22@users.noreply.github.com>
2023-05-21 15:50:23 -0500

    Add files via upload

---
+++ TiposDatos.java
@@ -0,0 +1,31 @@
+public class TiposDatos {
+    public static void main(String[] args) {
+        //Datos numericos enteros
+        byte numero1 = 6;
+        System.out.println(numero1);
+        short numero2 = 12;
+        System.out.println(numero2);
+        int numero3 = 188;
+        System.out.println(numero3);
+        long numero4 = 2247584;
+        System.out.println(numero4);
+
+        //Datos numericos decimales
+        float decimal1 = 7.5f;
+        System.out.println(decimal1);
+        double decimal2 = 10.7d;
+        System.out.println(decimal2);
+
+        //Datos tipo booleano
+        boolean tipo1 = true;
+        System.out.println(tipo1);
+        boolean tipo2 = false;
+        System.out.println(tipo2);
+
+        //Datos tipo texto
+        char nombre = 'C';
+        System.out.println(nombre);
+        String nombre1 = "Datos";
+        System.out.println(nombre1);
+    }
+}
